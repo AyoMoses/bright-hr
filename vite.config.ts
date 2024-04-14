d1aa826d455caed1e35cd775b0bb69a8a5fd8bd1
ayomoses <odukoyaayodeji@gmail.com>
2024-04-14 10:41:03 +0100

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bright-hr/',
+  // base: '/bright-hr/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
   },
-  build: {
-    outDir: 'dist',
-  },
+  // build: {
+  //   outDir: 'dist',
+  // },
 });
